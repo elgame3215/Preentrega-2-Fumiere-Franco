fe749588c5f3bb3f30b915fda3cf2088bfaf4300
Franco <francofumiere321@gmail.com>
2024-05-03 19:20:38 -0300

    añadida documentacion

--- js/classes.js
+++ js/classes.js
@@ -1,7 +1,7 @@
 class Player {
 	constructor( id ) {
 		this.id = id;
-		this.cards = [Math.ceil( Math.random() * 11 )];
+		this.cards = [Math.ceil( Math.random() * 11 )]; // el array de cartas se inicializa ya con una carta
 	}
 	takeCard() {
 		const playerNewCard = Math.ceil( Math.random() * 11 );
